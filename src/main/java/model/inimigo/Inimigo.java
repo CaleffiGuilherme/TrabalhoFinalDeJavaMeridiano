57a2bfeed38c1b0c2f731190ac434e7a755b44af
caleffiguilherme8@gmail.com <caleffiguilherme8@gmail.com>
2025-06-05 00:13:29 -0300

    Adicionando estrutura básica do model de Player e Inimigo

--- src/main/java/model/inimigo/Inimigo.java
+++ src/main/java/model/inimigo/Inimigo.java
@@ -1,4 +1,39 @@
 package model.inimigo;
 
+import model.player.Personagem;
+
 public class Inimigo {
+    protected String nome;
+    protected int vidaAtual;
+    protected int vidaMaxima;
+    protected int ataque;
+    protected int defesa;
+    protected int precisao;
+    protected String arma;
+
+    public Inimigo(String nome, int vidaAtual, int vidaMaxima, int ataque, int defesa, int precisao, String arma) {
+        this.nome = nome;
+        this.vidaAtual = vidaAtual;
+        this.vidaMaxima = vidaMaxima;
+        this.ataque = ataque;
+        this.defesa = defesa;
+        this.precisao = precisao;
+        this.arma = arma;
+    }
+
+    public void atacar(Inimigo personagem){
+        int dano = this.ataque - personagem.getDefesa();
+        if (dano < 0) dano = 0;
+        personagem.receberDano(dano);
+        System.out.println(this.nome + " atacou " + personagem.getNome() + " inferindo " + dano + " de dano.");
+    }
+
+    public void receberDano(int dano) {
+        this.vidaAtual -= dano;
+        if(this.vidaAtual <= 0) vidaAtual = 0;
+    }
+
+    public String getNome() { return nome; }
+    public int getDefesa() { return defesa; }
+
 }
--- src/main/java/model/player/Personagem.java
+++ src/main/java/model/player/Personagem.java
@@ -1,4 +1,39 @@
 package model.player;
 
-public class Personagem {
+public abstract class Personagem {
+        protected String nome;
+        protected int vidaAtual;
+        protected int vidaMaxima;
+        protected int ataque;
+        protected int defesa;
+        protected int esquiva;
+        protected int precisao;
+        protected String arma;
+
+        public Personagem(String nome, int vidaAtual, int vidaMaxima, int ataque, int defesa, int esquiva, int precisao, String arma) {
+                this.nome = nome;
+                this.vidaAtual = vidaAtual;
+                this.vidaMaxima = vidaMaxima;
+                this.ataque = ataque;
+                this.defesa = defesa;
+                this.esquiva = esquiva;
+                this.precisao = precisao;
+                this.arma = arma;
+
+        }
+
+        public void atacar(Personagem inimigo){
+                int dano = this.ataque - inimigo.getDefesa();
+                if (dano < 0) dano = 0;
+                inimigo.receberDano(dano);
+                System.out.println(this.nome + " atacou " + inimigo.getNome() + " inferindo " + dano + " de dano.");
+        }
+
+        public void receberDano(int dano) {
+                this.vidaAtual -= dano;
+                if(this.vidaAtual <= 0) vidaAtual = 0;
+        }
+
+        public String getNome() { return nome; }
+        public int getDefesa() { return defesa; }
 }
