c4657f3c3dbc7cde4c862770f0a4ba05048055bb
gabrielgoncalves06 <gabrielgoncalvespa0303@gmail.com>
2025-06-07 19:52:44 -0300

    Adicionei no controller o metodo InicializarFases

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -51,6 +51,34 @@ public class GameController {
         return true;
     }
 
+    public void inimigoAtaca(Inimigo inimigo) {
+        inimigo.atacar(player);
+    }
+
+    public boolean jogadorDerrotado() {
+        return player.getVidaAtual() <= 0;
+    }
+
+    public boolean inimigoDerrotado(Inimigo inimigo) {
+        return inimigo.getVidaAtual() <= 0;
+    }
+
+    private void inicializarFases() {
+        Missao saloon = new Missao("Saloon Abandonado", "Um velho bar cheio de teias de aranha.");
+        saloon.adicionarInimigo(new model.inimigo.Pistoleiro("Pistoleiro bêbado"));
+        saloon.adicionarItem(new Consumivel("Whisky", "Recupera 20 de vida", 1, Efeito.CURA, 20));
+
+        Missao mina = new Missao("Mina Escura", "Uma mina subterrânea, o ar é pesado.");
+        mina.adicionarInimigo(new model.inimigo.Pistoleiro("Pistoleiro sombrio"));
+        mina.adicionarInimigo(new model.inimigo.Pistoleiro("Pistoleiro das cavernas"));
+        mina.adicionarItem(new Consumivel("Dinamite", "Aumenta ataque em 10", 1, Efeito.AUMENTO_ATAQUE, 10));
+
+        fases.add(saloon);
+        fases.add(mina);
+    }
+
+
+
 
 
 }
